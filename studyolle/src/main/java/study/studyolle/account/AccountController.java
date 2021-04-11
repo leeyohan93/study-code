2224bbfc0171e7969b22ed45490deb38bad7d1b2
yohan <good_1411@naver.com>
2021-04-11 17:34:53 +0900

    스터디올래 - 회원가입 domain & view 구현

---
+++ studyolle/src/main/java/study/studyolle/account/AccountController.java
@@ -0,0 +1,17 @@
+package study.studyolle.account;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RequiredArgsConstructor
+@Controller
+public class AccountController {
+
+    @GetMapping("/sign-up")
+    public String signUpForm(Model model) {
+        model.addAttribute("signUpForm", new SignUpForm());
+        return "account/sign-up";
+    }
+}
---
+++ studyolle/src/main/java/study/studyolle/domain/Account.java
@@ -0,0 +1,58 @@
+package study.studyolle.domain;
+
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@EqualsAndHashCode(of = "id")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String nickname;
+
+    private String password;
+
+    private boolean emailVerified;
+
+    private String emailCheckToken;
+
+    private LocalDateTime joinedAt;
+
+    private String bio;
+
+    private String url;
+
+    private String occupation;
+
+    private String location;
+
+    @Lob
+    @Basic(fetch = FetchType.EAGER)
+    private String profileImage;
+
+    private boolean studyCreatedByEmail;
+
+    private boolean studyCreatedByWeb;
+
+    private boolean studyEnrollmentResultByEmail;
+
+    private boolean studyEnrollmentResultByWeb;
+
+    private boolean studyUpdateResultByEmail;
+
+    private boolean studyUpdateResultByWeb;
+}
