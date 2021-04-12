d32b57418fc2f83af8145736fc35e70f2560f2d5
yohan <good_1411@naver.com>
2021-04-12 11:34:13 +0900

    스터디올래 - 회원가입 폼 서브밋 검증

---
+++ studyolle/src/main/java/study/studyolle/account/AccountRepository.java
@@ -0,0 +1,10 @@
+package study.studyolle.account;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import study.studyolle.domain.Account;
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
+    boolean existsByEmail(String email);
+
+    boolean existsByNickname(String nickname);
+}
---
+++ studyolle/src/main/java/study/studyolle/account/SignUpFormValidator.java
@@ -0,0 +1,31 @@
+package study.studyolle.account;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@RequiredArgsConstructor
+@Component
+public class SignUpFormValidator implements Validator {
+
+    private AccountRepository accountRepository;
+
+    @Override
+    public boolean supports(final Class<?> clazz) {
+        return clazz.isAssignableFrom(SignUpForm.class);
+    }
+
+    @Override
+    public void validate(final Object target, final Errors errors) {
+        SignUpForm signUpForm = (SignUpForm) errors;
+        if(accountRepository.existsByEmail(signUpForm.getEmail())){
+            errors.rejectValue("email","invalid.email",new Object[]{signUpForm.getEmail()},"이미 등록된 이메일입니다.");
+        }
+
+        if(accountRepository.existsByNickname(signUpForm.getNickname())){
+            errors.rejectValue("email","invalid.email",new Object[]{signUpForm.getNickname()},"이미 등록된 닉네임입니다.");
+        }
+    }
+}
