49d96c03dac649440146c99f0e3ade7400004e36
Yohan Lee <good_1411@naver.com>
2021-04-27 12:30:58 +0900

    스터디 올래 - 스터디 도메인 구현

---
+++ studyolle/src/main/java/study/studyolle/study/domain/Study.java
@@ -0,0 +1,83 @@
+package study.studyolle.study.domain;
+
+import lombok.*;
+import study.studyolle.account.domain.Account;
+import study.studyolle.jone.domain.Zone;
+import study.studyolle.tag.domain.Tag;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Study {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(unique = true)
+    private String path;
+
+    private String title;
+
+    private String shortDescription;
+
+    private LocalDateTime publishedDateTime;
+
+    private LocalDateTime closedDateTime;
+
+    private LocalDateTime recruitingUpdatedDateTime;
+
+    private boolean recruiting;
+
+    private boolean published;
+
+    private boolean closed;
+
+    private boolean useBanner;
+
+    @Lob
+    @Basic(fetch = FetchType.EAGER)
+    private String fullDescription;
+
+    @Lob
+    @Basic(fetch = FetchType.EAGER)
+    private String image;
+
+    @ManyToMany
+    private Set<Account> managers = new HashSet<>();
+
+    @ManyToMany
+    private Set<Account> members = new HashSet<>();
+
+    @ManyToMany
+    private Set<Tag> tags = new HashSet<>();
+
+    @ManyToMany
+    private Set<Zone> zones = new HashSet<>();
+
+    @Builder
+    public Study(Long id, String path, String title, String shortDescription, LocalDateTime publishedDateTime,
+                 LocalDateTime closedDateTime, LocalDateTime recruitingUpdatedDateTime, boolean recruiting,
+                 boolean published, boolean closed, boolean useBanner, String fullDescription, String image) {
+        this.id = id;
+        this.path = path;
+        this.title = title;
+        this.shortDescription = shortDescription;
+        this.publishedDateTime = publishedDateTime;
+        this.closedDateTime = closedDateTime;
+        this.recruitingUpdatedDateTime = recruitingUpdatedDateTime;
+        this.recruiting = recruiting;
+        this.published = published;
+        this.closed = closed;
+        this.useBanner = useBanner;
+        this.fullDescription = fullDescription;
+        this.image = image;
+    }
+}
